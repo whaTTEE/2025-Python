ace7ce634e9f7b0570a7613d32a1d984887cb357
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-03-11 21:14:25 +0900

    Update Numpy01.py

--- Numpy01.py
+++ Numpy01.py
@@ -5,6 +5,8 @@ a = np.array([1,2,3], dtype = 'int32')
 # print(a)
 b = np.array([[9.0, 8.0, 7.0],[6.0, 5.0, 4.0]])
 # print(b)
-b.ndim // 차원의 수를 나타냄
+b.ndim #차원의 수를 나타냄
+a.dtype
+b.shape #2행,3열 행렬이다.
 
 
