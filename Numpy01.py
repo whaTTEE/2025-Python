c8ad08bb7f4ea37ac6fb1154220eec59b48a8ef5
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-03-11 21:12:12 +0900

    Create Numpy01.py

---
+++ Numpy01.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+
+a = np.array([1,2,3], dtype = 'int32')
+# print(a)
+b = np.array([[9.0, 8.0, 7.0],[6.0, 5.0, 4.0]])
+# print(b)
+b.ndim // 차원의 수를 나타냄
+
+
