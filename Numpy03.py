b57e46e1a64e4b08a082c201b5c1e831715a01c5
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-03-11 22:00:00 +0900

    Create Numpy03.py

---
+++ Numpy03.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+v1 = np.array([1,2,3,4])
+v2 = np.array([5,6,7,8])
+v3 = np.array([9,9,9,9])
+
+result = np.hstack([v1,v2,v3])
+print(result)
+print(" ")
+result = np.vstack([v1,v2,v3])
+print(result)
