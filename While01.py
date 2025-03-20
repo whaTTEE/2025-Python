250a4053242d311f0d5150be1f308d07696f1087
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-03-20 15:04:25 +0900

    Create While01.py

---
+++ While01.py
@@ -0,0 +1,13 @@
+# 2자리 양수( 10 ~ 99 ) 입력받기
+
+print("2자리 양수를 입력하세요.")
+
+while True:
+    no = int(input("값을 입력하세요: "))
+    if no >= 10 and no <= 99:
+        break
+    else:
+        print("10과 99사이의 수를 다시 입력하세요.")
+
+
+print(f"입력받은 양수는 {no}입니다.")
