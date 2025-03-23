68ba980f2505fe890e1b1405ba7b689ea0483dd3
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-03-24 08:31:21 +0900

    Create Average,Sum.py

---
+++ Average,Sum.py
@@ -0,0 +1,15 @@
+score1 = int(input("1번 학생의 점수를 입력하세요 : "))
+score2 = int(input("2번 학생의 점수를 입력하세요 : "))
+score3 = int(input("3번 학생의 점수를 입력하세요 : "))
+score4 = int(input("4번 학생의 점수를 입력하세요 : "))
+score5 = int(input("5번 학생의 점수를 입력하세요 : "))
+
+total = 0
+total += score1
+total += score2
+total += score3
+total += score4
+total += score5
+
+print(f"합계는 {total}점입니다.")
+print(f"평균은 {total/5}점입니다.")
