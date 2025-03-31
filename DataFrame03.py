e5d3b69c30a63551117b2b820592365452bb8ed3
2wnswoo <154565464+whaTTEE@users.noreply.github.com>
2025-03-31 22:36:01 +0900

    Create DataFrame03.py

---
+++ DataFrame03.py
@@ -0,0 +1,5 @@
+import numpy as np
+data = [{"Name" : "LEE", "Age" : 38, "Driver" : True},
+        {"Name" : "Kim", "Age" : 25, "Driver" : False}]
+        
+pd.DataFrame(data, index=["row1", "row2"])
